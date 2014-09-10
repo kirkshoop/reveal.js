8257f3c18d28fcf7662f0ba4ad5810b5533a8d8b
Kirk Shoop <kirk.shoop@microsoft.com>
2014-09-10 13:30:05 -0700

    include rxcpp

--- lib/cpp/sdl_mouse.cpp
+++ lib/cpp/sdl_mouse.cpp
@@ -1,6 +1,6 @@
 ///
 ///
-/// ./emscripten/incoming/em++ -std=c++11 -stdlib=libc++ -o hello.html -O2 ./sdl_mouse.cpp
+/// ~/emsdk_portable/emscripten/incoming/em++ -std=c++11 -stdlib=libc++ -o ./lib/hello/hello.html -O2 ./lib/cpp/sdl_mouse.cpp
 ///
 ///
 
@@ -12,7 +12,7 @@
 
 #include <cmath>
 
-#include "../../../../source/rxcpp/Rx/v2/src/rxcpp/rx.hpp"
+#include "rxcpp/rx.hpp"
 namespace rx=rxcpp;
 
 float float_map(
@@ -24,21 +24,22 @@ float float_map(
     return minResult + (maxResult - minResult) * ((value - minValue) / (maxValue - minValue));
 }
 
-typedef std::pair<float, float> point;
 
-point plus_point(const point& lhs, const point& rhs){
-  return point(lhs.first + rhs.first, lhs.second + rhs.second);
+SDL_Point operator+(const SDL_Point& lhs, const SDL_Point& rhs){
+  return SDL_Point{lhs.x + rhs.x, lhs.y + rhs.y};
 }
 
 SDL_Surface *screen;
+const SDL_Rect extent = {0, 0, 600, 250};
+const SDL_Point centered = {extent.w / 2, extent.h / 2};
 
-rx::subjects::behavior<point> centers(point(0,0));
+rx::subjects::behavior<SDL_Point> centers(centered);
 
 void render(){
-    boxRGBA(screen, 0, 0, 600, 450, 0xff, 0xff, 0xff, 0xff);
+    boxRGBA(screen, extent.x, extent.y, extent.w, extent.h, 0xff, 0xff, 0xff, 0xff);
 
     auto center = centers.get_value();
-    filledEllipseRGBA(screen, center.first, center.second, 20, 20, 0, 0, 0xff, 0xff);
+    filledEllipseRGBA(screen, center.x, center.y, 20, 20, 0, 0, 0xff, 0xff);
 
     SDL_UpdateRect(screen, 0, 0, 0, 0); 
 }
@@ -73,7 +74,7 @@ void main_2(void* arg);
 int main() {
   SDL_Init(SDL_INIT_VIDEO);
 
-  screen = SDL_SetVideoMode(600, 450, 32, SDL_HWSURFACE);
+  screen = SDL_SetVideoMode(extent.w, extent.h, 32, SDL_HWSURFACE);
 
   emscripten_async_call(main_2, NULL, 500); // avoid startup delays and intermittent errors
 
@@ -95,18 +96,20 @@ void main_2(void* arg) {
     map(
         [=](float t){
             // map the time value to a point on a circle
-            return point(orbit_radius * std::cos(t * 2 * 3.14), orbit_radius * std::sin(t * 2 * 3.14));
+            return SDL_Point{int(orbit_radius * std::cos(t * 2 * 3.14)), int(orbit_radius * std::sin(t * 2 * 3.14))};
         });
 
   auto mouse_positions = mouse_moves.
     get_observable().
     map(
-      [](const SDL_MouseMotionEvent& m){
-        return point(m.x, m.y);
-      });
+        [](const SDL_MouseMotionEvent& m){
+            return SDL_Point{m.x, m.y};
+        }).
+    publish(centered).
+    ref_count();
 
   mouse_positions.
-    combine_latest(plus_point, orbit_positions).
+    combine_latest(std::plus<SDL_Point>(), orbit_positions).
     subscribe(centers.get_subscriber());
 
   emscripten_set_main_loop(one, 0, 0);
